99a1096c6f2ce40d7819191b9f2dfd97a34e30d1
NaveenKumar075 <naveenkumarforjobs@gmail.com>
2025-02-11 23:44:13 +0530

    UPDATE: To pass the tempfile directly

--- main_app.py
+++ main_app.py
@@ -316,8 +316,8 @@ def main():
             if 'content' not in st.session_state:
                 st.warning("⚠ Please upload a document in ChatBot mode first.")
             else:
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf", mode="w", encoding="utf-8") as temp_file:
-                    temp_file.write(st.session_state.content)
+                with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+                    temp_file.write(uploaded_file.read())
                     temp_file_path = temp_file.name
                     
                 subprocess.run(["python", "EC_Summarization.py", temp_file_path])
